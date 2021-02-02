68b0e0fe3691e22ad1947dd4e8f6282785757dea
Shinedi <oo_julia@mail.ru>
2021-02-02 17:46:08 +0300

    Add fix js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 //https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 //Функция, возвращающая случайное целое число из переданного диапазона включительно.
 function getRandomIntInclusive(min, max) {
-  if (min<0){min*=-1;} //защита от отрицательного диапазона
-  if (max<0){max*=-1;}
-  if (min>max){[min,max]=[max,min];} //меняем местами в случае если ДО<ОТ
+  //защита от отрицательного диапазона
+  min = Math.abs(min);
+  max = Math.abs(max);
+
+  //меняем местами в случае если ДО<ОТ
+  if (min > max) {
+    [min, max] = [max, min];
+  }
 
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -15,6 +20,8 @@ getRandomIntInclusive(-666, 255);
 //Функция для проверки максимальной длины строки. Максимальная длина по проекту константа
 const MAX_LENGTH_COMMENT = 140;
 const checkLengthComment = function(comment, maxLength) {
-  return (comment <= maxLength && comment >= 0);
+  return comment.length <= maxLength;
 }
-checkLengthComment(-10, MAX_LENGTH_COMMENT);
+checkLengthComment('hfvhsdkhdkhdvbhsfsdknk,bnc,vcvjmxvnxvbmxbvmxnvbxmkcbvnxcbvxcvbxmcvbxbnvxmvbxmvbxnmdjkggggggvhdjkghnsdjkvghnvghnjsvnjsdkhsdjknvidnvuikehfldvnf', MAX_LENGTH_COMMENT);
+//141 символ
+checkLengthComment('hello', 60);
